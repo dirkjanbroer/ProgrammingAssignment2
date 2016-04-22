d00012a7b05e6ed442e1693cad7d8d67da97a1c6
Dirk Jan Broer <dirk.jan.broer@achmea.nl>
2016-04-22 11:32:25 +0200

    555
    Signed-off-by: unknown <AA009738@C0940503.eur.pontw>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix stores the given matrix and the inverse of the given matrix in 2 different objects, usable outside the function
 
-## Write a short comment describing this function
+## cacheSolve reads the inverse of the given matrix out of memory when it is the same as the matrix given to makeCacheMatrix
 
-makeCacheMatrix <- function(x = matrix()) {
+## cacheSolve calculates the inverse of the given matrix when it differs from the matrix given to makeCacheMatrix
+
+
+
+## this function creates an object m, usable outside the function, which contains the given matrix
+
+## and it stores the inverse of the given matrix in an object n, also usable outside the function
+make
+CacheMatrix <- function(x = matrix()){
+
+    m <<- x
+  
+    n <<- solve(m)
 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## If the inverse has already been calculated (and the matrix has not changed)
+
+##    then this function retrieves the inverse from the cache
+
+##    else this function calculates the inverse of the new given matrix
+
+cacheSolve <- function(o){
+
+## If the inverse has already been calculated and the matrix has not changed
+
+  if(exists("n") && identical(o,m))  {
+
+    return(n)
+
+  }
+
+  else    {
+
+    return(solve(o))
+
+  }
+
 }
